1a901a01e5ec2ddde4f854787bb4878780d7e4b4
Lu-Smith <104765947+Lu-Smith@users.noreply.github.com>
2024-09-21 17:52:07 +0100

    Created wordlist.py

---
+++ wordslist.py
@@ -0,0 +1,24 @@
+# words for hangman game
+
+words = ("apple", "orange", "strawberry", "pear", 
+         "banana", "pineapple", "grape", "mango", 
+         "blueberry", "kiwi", "watermelon", "peach", 
+         "cherry", "plum", "raspberry", "papaya"
+         "dog", "cat", "elephant", "giraffe", "tiger", "lion", "kangaroo", 
+         "panda", "zebra", "dolphin",
+         "Africa", "Asia", "Europe", "Australia", "Antarctica",
+         "London", "Paris", "Tokyo", "Berlin", "Sydney", 
+         "Rome", "Toronto", "Dubai", "Istanbul", "Bangkok", "Singapore", 
+         "Barcelona", "Moscow", "Chicago", "Seoul", "Mumbai",
+         "Canada", "Brazil", "Germany", "France", 
+         "Japan", "Australia", "India", "Mexico", "China",
+         "chair", "book", "phone", "laptop", "bottle", "guitar", 
+         "lamp", "ball", "shoes", "clock", "pencil",  
+         "umbrella", "scissors", "plate", 
+         "door", "window", "soap", "mirror", "bicycle", "gloves", 
+         "fork", "spoon", "camera", "wallet",
+         "notebook", "computer", "elephant", "painting", "sandwich", 
+         "brochure", "avocados", "dinosaur", "umbrella", "chocolate",
+         "station", "candles", "brother", "fortune", "picture", 
+         "orchard", "respect", "explore", "laundry", "reading"
+    )
