119651e53c9988a4a0b52df3237c8a9e1cfc7814
Lu-Smith <104765947+Lu-Smith@users.noreply.github.com>
2024-09-21 16:26:36 +0100

    Created hangman.py

---
+++ hangman.py
@@ -0,0 +1,17 @@
+# Hangman in Python
+
+words = ("apple", "orange", "strawberry", "pear", 
+         "banana", "pineapple", "grape", "mango", 
+         "blueberry", "kiwi", "watermelon", "peach", 
+         "cherry", "plum", "raspberry", "papaya")
+
+# dictionary of key:()
+hangman_art = {0: ("   "
+                   "   "
+                   "   "),
+               1: (),
+               2: (),
+               3: (),
+               4: (),
+               5: (),
+               6: ()}
