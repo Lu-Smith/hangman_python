9ca0c4f08f55d31eabb16cdcfbc362664a911782
Lu-Smith <104765947+Lu-Smith@users.noreply.github.com>
2024-09-21 17:14:54 +0100

    Updated hangman.py logic

--- hangman.py
+++ hangman.py
@@ -35,22 +35,28 @@ def display_man(wrong_guesses):
   print("*******")
 
 def display_hint(hint):
-  pass
+  print(" ".join(hint))
 
 def display_answer(answer):
-  pass
+  print(" ".join(answer))
 
 def main():
   answer = random.choice(words)
   hint = ["_"] * len(answer)
-  wrong_guesses = 5
+  wrong_guesses = 0
   gussed_letter = set()
   is_running = True
   
   while is_running:
     display_man(wrong_guesses)
     display_hint(hint)
-    guess = input("Enter a letter: ").upper()
+    display_answer(answer)
+    guess = input("Enter a letter: ").lower()
+    
+    if guess in answer:
+      for i in range(len(answer)):
+        if answer[i] == guess:
+          hint[i] = guess
 
 if __name__ == "__main__":
   main()
